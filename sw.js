d0a02700c8a6a1faf047348a56ed6e3f02b60367
stinkipadel <stinkipinki97@gmail.com>
2026-04-18 19:15:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// RacketAI Service Worker v3.32
+// RacketAI Service Worker v3.33
 // Aggiorna CACHE_NAME ad ogni release per forzare il refresh
 
-const CACHE_NAME = 'racketai-v3.32';
+const CACHE_NAME = 'racketai-v3.33';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
